1fa5c22d3443c5a5a096594e49730559065cafa7
Ryo Sato <ryo.sato@speee.jp>
2021-05-17 14:27:37 +0900

    add gasreport

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import 'hardhat-typechain'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
+import 'hardhat-gas-reporter'
 
 const LOW_OPTIMIZER_COMPILER_SETTINGS = {
   version: '0.7.6',
@@ -77,5 +78,10 @@ export default {
       'contracts/NonfungibleTokenPositionDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
       'contracts/libraries/NFTDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
     },
+  },
+  gasReporter: {
+    currency: 'JPY',
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    outputFile: 'gasreport.log',
   },
 }
